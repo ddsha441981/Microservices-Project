0dae5141f71dd41078e7ad91dbe0d07f550de035
ddsha441981 <ddsa44191@gmail.com>
2023-02-09 08:41:36 +0530

    Implemented CircuitBreaker Resilience4j
    CircuitBreaker Resilience4j integreted

--- UserService/src/main/java/com/cwc/user/service/controller/UserController.java
+++ UserService/src/main/java/com/cwc/user/service/controller/UserController.java
@@ -15,30 +15,52 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cwc.user.service.entities.User;
 import com.cwc.user.service.services.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
 	@Autowired
 	private UserService userService;
-	//create user
+
+	// create user
 	@PostMapping("/")
-	public ResponseEntity<User> createUser(@RequestBody User user){
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User savedUser = userService.saveUser(user);
+		log.info("Create New User Handler : UserController");
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
 	}
-	//get single user
+
+	// get single user
 	@GetMapping("/{userId}")
-	public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+	@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelBreaker")
+	public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
 		User user = userService.getUser(userId);
+		log.info("Get Single User Handler : UserController");
 		return ResponseEntity.ok(user);
 	}
-	
-	//get all user
+
+	// CircuitBreaker Resilience4j fallback method
+	public ResponseEntity<User> ratingHotelBreaker(String userId, Exception ex) {
+		log.info("Fallback is executed : Some Service is Down", ex.getMessage());
+		User user = User.builder()
+				.name("dammy User")
+				.email("dammy@gmail.com")
+				.about("Dammy User is created beacause some service is down")
+				.userId("123456789")
+				.build();
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
+
+	// get all user
 	@GetMapping("/")
-	public ResponseEntity<List<User>> getAllUsers(){
+	public ResponseEntity<List<User>> getAllUsers() {
 		List<User> allUser = userService.getAllUser();
+		log.info("Get All User Handler : UserController");
 		return ResponseEntity.ok(allUser);
-		
+
 	}
 }
